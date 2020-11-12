6ed66669ffd425e9a098711f0ba700199166e671
Vikram <vs00saini@gmail.com>
2020-11-12 08:11:38 +0530

    Write start test function

--- YoYo-Web-App/wwwroot/js/site.js
+++ YoYo-Web-App/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+"use strict";
+
+var app = {
+
+    init: () =>
+    {
+        console.log("Working");
+    },
+
+    startTest: () =>
+    {
+        //console.log(event.target);
+
+        $("#btnPlay").addClass("d-none");
+        $("#btnPlayInfo").removeClass("d-none");
+    },
+
+    request: (url, data, successCallback) =>
+    {
+        $.ajax({
+            type: "GET",
+            data: data,
+            url: url,
+            success: function (response)
+            {
+                successCallback(response);
+            },
+            error: function (xhr)
+            {
+                const errorMessage = `${xhr.status}:${xhr.statusText}`;
+                console.error(errorMessage);
+                console.log(xhr.responseText);
+            }
+        });
+    }
+};
